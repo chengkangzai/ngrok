15d538ed929da1ed92b8eaeff6e70d9ef0254ac0
chengkangzai <pycck@hotmail.com>
2019-08-12 23:06:45 +0800

    Initial Comit With Password update

---
+++ Password update.ps1
@@ -0,0 +1,4 @@
+﻿$password = "Your Frikin Password"
+$secureStringPwd = $password | ConvertTo-SecureString -AsPlainText -Force 
+$secureStringText = $secureStringPwd | ConvertFrom-SecureString 
+Set-Content "C:\ngrok\email.cred" $secureStringText
---
+++ main.ps1
@@ -0,0 +1,9 @@
+﻿Function Internet{
+if (Test-Connection www.google.com -Quiet) {} {netsh wlan connect name="VC-A1-10-06" interface="Wi-Fi"}
+}
+
+Internet 
+ 
+start C:\ngrok\ngrok.exe "tcp 3389 -region=ap" -WindowStyle Hidden 
+
+powershell C:\ngrok\updatengrokserver.ps1
