514128a5af1f5b82bddba113ccee6d58591bdd85
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2019-12-26 22:50:40 +0800

    Update Password update.ps1

--- Password update.ps1
+++ Password update.ps1
@@ -1,4 +1,16 @@
-﻿$password = "Your Frikin Password"
+function CreateEmailCredential {
+    $path = ".\email.cred"
+    if (Test-Path $path -PathType Leaf) {
+        New-Item -Path $path -Type File
+        Set-Content $path $secureStringText    
+    }
+    else {
+        Set-Content $path $secureStringText    
+    }
+}
+
+$password = "Your Frikin Password"
 $secureStringPwd = $password | ConvertTo-SecureString -AsPlainText -Force 
 $secureStringText = $secureStringPwd | ConvertFrom-SecureString 
-Set-Content "C:\ngrok\email.cred" $secureStringText
+CreateEmailCredential
+
