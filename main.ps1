57eeb4e7f9d323a14bf8cb60bef499bbbdb99da7
chengkangzai <pycck@hotmail.com>
2019-12-26 12:05:44 +0800

    spamming

--- main.ps1
+++ main.ps1
@@ -8,10 +8,26 @@ function StartNgrok {
         Send-Discord -port $port 
         Send-Email -port $port
     }else {
-        $port = Get-NgrokPort 
-        Send-Discord -port $port -Heartbeat 1
-        Send-Email -port $port -Heartbeat 1
+        $start =Get-Date -Hour 12 -Minute 0 -Second 0
+        $end =Get-Date -Hour 12 -Minute 30 -Second 0
+        $start1 =Get-Date -Hour 00 -Minute 0 -Second 0
+        $end1 =Get-Date -Hour 00 -Minute 30 -Second 0
+        $now= Get-Date
+        if ($now -ge $start -and $now -le $end ) {
+            Write-Host "It's in Spamming Area... Spamming Started"
+            $port = Get-NgrokPort 
+            Send-Discord -port $port -Heartbeat 1
+            Send-Email -port $port -Heartbeat 1
+        }elseif ($now -ge $start1 -and $now -le $end1) {
+            Write-Host "It's in Spamming Area... Spamming Started"
+            $port = Get-NgrokPort 
+            Send-Discord -port $port -Heartbeat 1
+            Send-Email -port $port -Heartbeat 1
+        } else {
+            Write-Host "Avoid Spamming"
+        }
     }
+    
 }
 
 $stat = $true
