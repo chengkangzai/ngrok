3ac583809fe95f65f1b34badfa936fab8cd1f90f
chengkangzai <pycck@hotmail.com>
2019-12-27 23:58:06 +0800

    Fix problem where password will not be save

--- Password update.ps1
+++ Password update.ps1
@@ -1,17 +1,22 @@
 function CreateEmailCredential {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $secureStringText
+    )
     $path = ".\email.cred"
     if (Test-Path $path -PathType Leaf) {
-        New-Item -Path $path -Type File
         Set-Content $path $secureStringText    
     }
     else {
+        New-Item -Path $path -Type File
         Set-Content $path $secureStringText    
     }
 }
 $password = Read-Host 'What is your password?' -AsSecureString
-$secureStringPwd = $password | ConvertTo-SecureString -AsPlainText -Force 
-$secureStringText = $secureStringPwd | ConvertFrom-SecureString 
-CreateEmailCredential
+$secureStringText = $password | ConvertFrom-SecureString 
+CreateEmailCredential -secureStringText $secureStringText
 
 #Reference https://blog.kloud.com.au/2016/04/21/using-saved-credentials-securely-in-powershell-scripts/
 
