5062496284bdcd9965e7251243d90e7db5ec0227
chengkangzai <pycck@hotmail.com>
2020-01-13 17:54:07 +0800

    Change timer from json

--- spam.ps1
+++ spam.ps1
@@ -1,16 +1,19 @@
 $config=Get-Content .\setup.json | ConvertFrom-Json
-Set-Location $config.dirPath
+$dir=$config.dirPath
+$sleepTime = $config.spamTimeInSecond 
+Write-Host "Setting Location to $dir"
+Set-Location $dir
 
 Import-Module ".\core.psm1" -DisableNameChecking
 
 function StartNgrok {
-    Write-Host "ngrok is running Running "
     $process = (Get-Process -Name ngrok -ErrorAction SilentlyContinue).Count
     if ($process -eq 0) {
         Write-Host "Initiate NGROK now"
         Start-Process .\ngrok.exe "tcp 3389 -region=ap" -WindowStyle Hidden  
         Send-Restart
     }else{
+        Write-Host "Ngrok is started"
         Send-Spam
     }        
 }    
@@ -18,9 +21,8 @@ function StartNgrok {
 $stat = $true
 while ($stat) {
     if (Test-Connection www.google.com -Quiet) {
-        Write-Host "Network Connection detected!  Proceed to start ngrok"
+        Write-Host "Network Connection detected!"
         StartNgrok
-        $sleepTime=300
         Write-Host "Start Sleep for $sleepTime second"
         Start-Sleep $sleepTime
     }
